ba48a6886aa4f663a0a34d940c952e71bbb68c1b
eunj <dmswjd362@gmail.com>
2014-09-12 15:38:39 +0900

    수정

---
+++ script.js
@@ -0,0 +1,19 @@
+ var game = new Phaser.Game(1920, 1080, Phaser.AUTO, '', {preload: preload, create: create}); 
+var cow;
+ function preload() {
+ 	game.load.spritesheet('cow', 'assets/cow.png', 100, 50);
+}
+
+function create() {
+	cow = game.add.sprite(100.100, 'cow');
+	cow.animations.add('walk');
+	cow.inputEnabled = true;
+	cow.events.onInputDown.add(cowClick, this);
+
+	//cow.animations.play('walk', 5, true);
+
+}
+
+ function cowClick() {
+  cow.animations.play('walk', 5, false);
+}
