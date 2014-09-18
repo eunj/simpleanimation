a69473e7d570f9bbdd45236689e70fa09a1417f5
eunj <dmswjd362@gmail.com>
2014-09-18 19:30:39 +0900

    수정

--- script.js
+++ script.js
@@ -66,7 +66,7 @@ function create() {
 	trowel = game.add.sprite(130,690, 'trowel');
 	trowel.animations.add('movetrowel');
 
-	uidrum.mo = game.add.tween(uidrum)
+	trowel.mo = game.add.tween(trowel)
 		.to({ x: 130, y: 680 }, 2000, Phaser.Easing.Linear.None)
 		.to({ x: 430, y: 600 }, 2000, Phaser.Easing.Linear.None)
 		.loop()
